1331a6e7923007f08ffd55e3654833739867eccc
Donevan Gonzales <47193440+DonevanGonzales@users.noreply.github.com>
2020-03-30 19:33:32 -0500

    Add files via upload

--- junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -38,4 +38,44 @@ class CalculatorTests {
 		assertEquals(expectedResult, calculator.add(first, second),
 				() -> first + " + " + second + " should equal " + expectedResult);
 	}
+
+    @Test
+	@DisplayName("3 - 2 = 1")
+	void subTwoNumbers() {
+		Calculator calculator = new Calculator();
+		assertEquals(1, calculator.sub(3, 2), "3 - 2 should equal 1");
+	}
+
+	@ParameterizedTest(name = "{0} - {1} = {2}")
+	@CsvSource({
+			"1,    1,   0",
+			"3,    2,   1",
+			"52,  51, 1",
+			"100,  1, 99"
+	})
+	void sub(int first, int second, int expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.sub(first, second),
+				() -> first + " - " + second + " should equal " + expectedResult);
+	}
+
+    @Test
+	@DisplayName("8 - 4 = 4")
+	void sub1TwoNumbers() {
+		Calculator calculator = new Calculator();
+		assertEquals(4, calculator.sub(8, 4), "8 - 4 should equal 4");
+	}
+
+	@ParameterizedTest(name = "{0} - {1} = {2}")
+	@CsvSource({
+			"5,    1,   4",
+			"2,    1,   1",
+			"60,  51, 9",
+			"30,  10, 20"
+	})
+	void sub1(int first, int second, int expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.sub(first, second),
+				() -> first + " - " + second + " should equal " + expectedResult);
+	}
 }
